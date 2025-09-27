5f17b7d8ff593b9711482fae0f06a5c4dd0e85ad
kannikii <kannikii0124@gmail.com>
2025-09-27 20:39:42 +0900

    Now, can delete comments!

--- app.js
+++ app.js
@@ -131,6 +131,26 @@ app.post("/write-comment",async(req,res)=>{
     postService.updatePost(collection,id,post);
     return res.redirect(`/detail/${id}`);
 });
+
+//댓글 삭제
+app.delete("/delete-comment",async(req,res)=>{
+    const { id,idx,password}=req.body;
+
+    const post = await collection.findOne(
+        {
+            _id:ObjectId(id),
+            comments: {$elemMatch: {idx: parseInt(idx),password}},
+        },
+        postService.projectionOption,
+    );
+    if(!post) {
+        return res.json({isSuccess:false});
+    }
+    post.comments = post.comments.filter((comment)=>comment.idx!=idx);
+    postService.updatePost(collection,id,post);
+    return res.json({isSuccess:true});
+});
+
 let collection;
 app.listen(3000, async() =>{
     console.log("Server started");
