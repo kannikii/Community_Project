54d959b6443edc6d4ed00124de61639991c9c43b
kannikii <kannikii0124@gmail.com>
2025-09-29 15:53:25 +0900

    Modify environment variable for monogodb connection.

--- configs/mongodb-connection.js
+++ configs/mongodb-connection.js
@@ -1,5 +1,5 @@
 const {MongoClient} = require("mongodb");
-const uri = "mongodb+srv://kwnnh0124_db_user:hyos0124@cluster0.4qhrcgb.mongodb.net/board";
+const uri = process.env.MONGODB_URI;
 
 module.exports=function(callback) {
     return MongoClient.connect(uri,callback);
